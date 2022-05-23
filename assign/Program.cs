be402e3a801baf6600eb73e138abe4e2c99c2724
farzane <farzanefatemi22@yahoo.com>
2022-05-23 13:35:30 +0430

    third assignment

---
+++ assign/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace session1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int sum=0;
+           Console.WriteLine("inter your favorite number:");
+           int number=Convert.ToInt32(Console.ReadLine());
+           
+
+           for (int i = 1; i <= number ; i++)
+           {
+            if(i%2==1)
+             {
+               sum=sum+i; 
+                 
+             } 
+               
+             
+           }
+           Console.WriteLine($"sum is:{sum}");
+
+        }
+    
+     }
+ }
